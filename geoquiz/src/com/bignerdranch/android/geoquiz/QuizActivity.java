6a6e730f817bc7529d06a53f1d9e6a6a50728d28
Vinit Samel <vinitsamel@gmail.com>
2013-06-17 16:43:10 -0700

    chapter 3

--- geoquiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ geoquiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.geoquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +15,8 @@ public class QuizActivity extends Activity {
 	private Button mFalseButton;
 	private Button mNextButton;
 	private TextView mQTextView;
+	private static final String TAG = "QuizActivity";
+	private static final String CURRENTINDEX = "CurrentIndex";
 	
 	private TrueFalse[] mQuestionBank = new TrueFalse[] {
 			new TrueFalse(R.string.q_2, false),
@@ -28,6 +31,9 @@ public class QuizActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_quiz);
+		Log.d(TAG, "onCreate()");
+		
+		if (null != savedInstanceState) mCurrentIndex = savedInstanceState.getInt(CURRENTINDEX, 0);
 		
 		mTrueButton = (Button) findViewById(R.id.true_button);
 		mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -78,6 +84,43 @@ public class QuizActivity extends Activity {
 	private void updateQuestion() {
 		int questionId = mQuestionBank[mCurrentIndex].getQuestion();
 		mQTextView.setText(questionId);	
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		Log.d(TAG, "onDestroy()");
+	}
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		Log.d(TAG, "onStart()");
+	}
+	
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Log.d(TAG, "onStop()");
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Log.d(TAG, "onPause()");
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Log.d(TAG, "onResume()");
+	}
+	
+	@Override
+	protected void onSaveInstanceState(Bundle savedInstanceState) {
+		super.onSaveInstanceState(savedInstanceState);
+		Log.d(TAG, "onSaveInstanceState with (K,V) = (" + CURRENTINDEX + ", " + mCurrentIndex + ")");
+		savedInstanceState.putInt(CURRENTINDEX, mCurrentIndex);
 	}
 
 }
